0c1e422d8b6c6e017276981c1ec985d0b5bce57b
kirill <kirusha1991@gmail.com>
2021-10-03 23:58:41 +0300

    lesson 4 homework

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 
 from core.models import MyUser
 from core.serializers import MyUserSerializer
 
 
-class MyUserViewSet(ModelViewSet):
+class MyUserViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
     queryset = MyUser.objects.all()
     serializer_class = MyUserSerializer
