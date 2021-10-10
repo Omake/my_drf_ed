e2e723d28de23e5317cf61e88e9eb6a7f6f8e7e6
kirill <kirusha1991@gmail.com>
2021-10-10 21:27:51 +0300

    lesson 6 homework

--- my_drf_ed/settings.py
+++ my_drf_ed/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'corsheaders',
+
     'core',
     'todo',
 ]
@@ -144,4 +145,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
