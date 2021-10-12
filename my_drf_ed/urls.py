acf8176343d660870b11cf4124324b502e208077
kirill <kirusha1991@gmail.com>
2021-10-12 18:50:33 +0300

    lesson 6 homework bugfix

--- my_drf_ed/urls.py
+++ my_drf_ed/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from core.views import MyUserViewSet
 from todo.views import ProjectViewSet, TodoViewSet
@@ -27,6 +28,6 @@ router.register('project', ProjectViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
     path('api/', include(router.urls)),
 ]
